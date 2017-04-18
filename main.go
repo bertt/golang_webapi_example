c1dfa42a42aed911bc7cf3ddb03b4d4d93f0afcf
bert <btemme@hotmail.com>
2017-04-18 15:35:48 +0200

    initial code

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"github.com/gorilla/mux"
+	"encoding/json"
+)
+
+type User struct {
+	Id           uint32 `json:"id"`
+	Username     string `json:"username"`
+	MoneyBalance uint32 `json:"balance"`
+}
+
+var userStore = []User{}
+
+func listUsersHandler(w http.ResponseWriter, r *http.Request) {
+	users, _ := json.Marshal(userStore)
+	w.Write(users)
+}
+
+func createUserHandler(w http.ResponseWriter, r *http.Request) {
+	users, _ := json.Marshal(userStore)
+	w.Write(users)
+}
+
+func Handlers() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", listUsersHandler).Methods("GET")
+	r.HandleFunc("/users", createUserHandler).Methods("POST")
+	return r
+}
+func main(){
+	var userBob = User{0, "Bob",100}
+	res = append(userStore,userBob)
+	fmt.Println("Server starting")
+	http.ListenAndServe(":8000", Handlers())
+}
